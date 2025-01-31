93d2081c45f125fbd17912b41dffe2793952b625
KrystofBo <krystof.bobek1@gmail.com>
2025-01-31 20:55:03 +0100

    experiment settings adjustments

--- run_donut_gini.sh
+++ run_donut_gini.sh
@@ -7,9 +7,9 @@ else
   echo "Folder already exists: $FOLDER_PATH"
 fi
 
-python main.py -env donut -net linear -ep 500 -nexp 10 -sm binary -cf True -rt gini -root datasets/gini_donut/
+python main.py -env donut -net linear -ep 500 -nexp 10 -sm binary -bs 2048 -cf True -rt gini -root datasets/gini_donut/
 python main.py -env donut -net linear -ep 500 -nexp 10 -sm binary -bs 64 -rt gini -root datasets/gini_donut/
-python main.py -env donut -net linear -ep 500 -nexp 10 -sm reset-binary -rt gini -root datasets/gini_donut/
-python main.py -env donut -net linear -ep 500 -nexp 10 -sm equal-binary -rt gini -root datasets/gini_donut/
+python main.py -env donut -net linear -ep 500 -nexp 10 -sm reset-binary -bs 64 -rt gini -root datasets/gini_donut/
+python main.py -env donut -net linear -ep 500 -nexp 10 -sm equal-binary -bs 64 -rt gini -root datasets/gini_donut/
 python main.py -env donut -net rnn -ep 500 -nexp 10 -sm rnn -bs 256 -lr 0.002 -rt gini -root datasets/gini_donut/
 python create_plots.py --env donut --root datasets/gini_donut --smooth 10 --filename gini_donut
